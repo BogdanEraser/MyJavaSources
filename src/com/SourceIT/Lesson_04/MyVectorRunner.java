ce7c0fb8b6ab1b99d9e9486a6f46a016dec7a4e5
BogdanEraser <erasermail2008@gmail.com>
2015-08-06 15:02:15 +0300

    from netbook

--- src/com/SourceIT/Lesson_04/MyVectorRunner.java
+++ src/com/SourceIT/Lesson_04/MyVectorRunner.java
@@ -6,13 +6,19 @@ package com.SourceIT.Lesson_04;
 public class MyVectorRunner {
     public static void main(String[] args) {
         Vector mas = new Vector();
-        mas.add("qwe0");
+        mas.add("qwe0");        //добавляем элементы
         mas.add("asd1");
         mas.add("zxc2");
-        System.out.println();
-        mas.toString();
-        System.out.println();
-        System.out.println(mas);
+        mas.add("rty3");
+        System.out.println(mas);    //отображаем
+        int j = 1;  //задаем произвольное j
+        System.out.println("Элемент " + j + " = " + mas.showElement(j)); //получаем j-й элемент
 
+        mas.deleteElement(j);       //удаляем j-й элемент
+        System.out.println(mas);    //отображаем
+
+        System.out.println("Элемент " + j + " = " + mas.showElement(j)); //получаем j-й элемент
+        mas.destroy();  //очищаем
+        System.out.println(mas);    //отображаем
     }
 }
--- src/com/SourceIT/Lesson_04/Vector.java
+++ src/com/SourceIT/Lesson_04/Vector.java
@@ -41,26 +41,51 @@ public class Vector {
 
     //очистка массива
     public void destroy() {
-//        for (int i = 0; i < this.v.length; i++) {
-//            this.v[i] = null;
-//        }
         this.quantity = 0;
         this.v = null;
     }
 
-
     //добавление элемента
     public void add(String s) {
-        if (this.quantity == 0) {this.v[0] = s;}
-        else {
-            String[] tmp = new String[this.quantity+1];
-            for (int i = 0; i < this.v.length; i++) {
-                tmp[i] = this.v[i];
-            }
-            tmp[this.quantity+1] = s;
+        if (this.quantity == 0) {
+            this.v[0] = s;
+        } else {
+            String[] tmp = new String[this.v.length + 1];
+            System.arraycopy(this.v, 0, tmp, 0, this.v.length);
+            tmp[this.v.length] = s;
             this.v = tmp.clone();
+            tmp = null;   //не уверен, что нужно, но зачем-то обнуляю ссылку на временный массив
         }
         this.quantity++;
+    }
+
+    //получение j-го элемента
+    public String showElement(int j) {
+        if (j >= this.v.length) {
+            return "Данный элемент недоступен";
+        }
+        if (this.quantity == 0) {return "Массив пуст";}
+        else {
+            return this.v[j];
+        }
+    }
+
+    //удаление j-го элемента
+    public void deleteElement(int j) {
+        if ((j >= 0) && (j < this.v.length)) {
+            if (this.v.length == 1) {
+                this.v[0] = null; //удаление единственного, 0-го элемента
+                this.quantity = 0;
+            }
+            else {
+                String[] tmp = new String[this.v.length-1];
+                System.arraycopy(this.v, 0, tmp, 0, j);
+                System.arraycopy(this.v, j+1, tmp, j, this.v.length-(j+1));
+                this.v = tmp.clone();
+                tmp = null;   //не уверен, что нужно, но зачем-то обнуляю ссылку на временный массив
+                this.quantity--;
+            }
+        }
     }
 
     @Override
