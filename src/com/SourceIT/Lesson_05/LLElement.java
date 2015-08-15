ad1bb6c5f25bd1a977b88bac08d0843364285f78
BogdanEraser <erasermail2008@gmail.com>
2015-08-15 12:16:34 +0300

    from netbook

---
+++ src/com/SourceIT/Lesson_05/LLElement.java
@@ -0,0 +1,43 @@
+package com.SourceIT.Lesson_05;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Bogdan Kukharskiy on 15.08.2015.
+ */
+public class LLElement<T> {
+    private T value;
+    LLElement<T> linkToNext;
+    LLElement<T> linkToPrev;
+
+    //конструктор
+    public LLElement(T value, LLElement<T> linkToNext, LLElement<T> linkToPrev) {
+        this.value = value;
+        this.linkToNext = linkToNext;
+        this.linkToPrev = linkToPrev;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public LLElement<T> getLinkToNext() {
+        return linkToNext;
+    }
+
+    public void setLinkToNext(LLElement<T> linkToNext) {
+        this.linkToNext = linkToNext;
+    }
+
+    public LLElement<T> getLinkToPrev() {
+        return linkToPrev;
+    }
+
+    public void setLinkToPrev(LLElement<T> linkToPrev) {
+        this.linkToPrev = linkToPrev;
+    }
+}
